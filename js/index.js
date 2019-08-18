6fcf20ffa34f8af66fb9b087bda9869e4bc39609
Tammy Metz <tammy@tammymetz.com>
2019-08-18 04:51:30 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ function showRepositories() {
 
 function getRepositories() {
   const req = new XMLHttpRequest();
-  req.addEventListener('load', showRepositories;
+  req.addEventListener('load', showRepositories);
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
 }
